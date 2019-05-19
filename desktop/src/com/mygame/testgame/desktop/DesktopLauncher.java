104c86822f3f607e493f93f45517607fb6364f8f
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2019-05-19 09:35:11 +0530

    files added

---
+++ desktop/src/com/mygame/testgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygame.testgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygame.testgame.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
